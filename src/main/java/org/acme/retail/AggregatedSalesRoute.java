e4845924907c05683c3f82afd4419063225ad418
Bernard Tison <bernard.tison@gmail.com>
2022-05-12 10:46:18 +0200

    feature: handle primary key violation error, send expense created/updated to Kafkaevent

--- src/main/java/org/acme/retail/AggregatedSalesRoute.java
+++ src/main/java/org/acme/retail/AggregatedSalesRoute.java
@@ -11,6 +11,8 @@ import io.vertx.core.json.JsonObject;
 import org.apache.camel.LoggingLevel;
 import org.apache.camel.Message;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.kafka.KafkaConstants;
+import org.postgresql.util.PSQLException;
 
 @ApplicationScoped
 public class AggregatedSalesRoute extends RouteBuilder {
@@ -25,17 +27,54 @@ public class AggregatedSalesRoute extends RouteBuilder {
                 .log(LoggingLevel.DEBUG, "Sale event received: ${body}")
                 .process(exchange -> {
                     Message in = exchange.getIn();
-                    JsonObject json = new JsonObject(in.getBody(Map.class));
-                    in.setBody(new StringBuilder()
-                            .append("INSERT INTO expense (sale_id, customer_id, amount, date) VALUES (")
-                            .append(json.getLong("sale_id")).append(", ")
-                            .append(json.getLong("customer_id")).append(", ")
-                            .append(json.getString("total")).append(", ")
-                            .append("TIMESTAMP '").append(transformEpochToDateTime(json.getLong("date") / 1000))
-                            .append("'").append(");"));
+                    in.setHeader("sale_id", in.getBody(Map.class).get("sale_id"));
+                    in.setHeader("total", in.getBody(Map.class).get("total"));
                 })
-                .log(LoggingLevel.DEBUG, "SQL statement: ${body}")
-                .to("jdbc:cashback");
+                .doTry()
+                    .process(exchange -> {
+                        Message in = exchange.getIn();
+                        JsonObject json = new JsonObject(in.getBody(Map.class));
+                        in.setBody(new StringBuilder()
+                                .append("INSERT INTO expense (sale_id, customer_id, amount, date) VALUES (")
+                                .append(json.getLong("sale_id")).append(", ")
+                                .append(json.getLong("customer_id")).append(", ")
+                                .append(json.getString("total")).append(", ")
+                                .append("TIMESTAMP '").append(transformEpochToDateTime(json.getLong("date") / 1000))
+                                .append("'").append(");"));
+                        in.setHeader("op", "c");
+                    })
+                    .log(LoggingLevel.DEBUG, "SQL statement: ${body}")
+                    .to("jdbc:cashback")
+                    .to("direct:kafka")
+                .doCatch(PSQLException.class)
+                    .process(exchange -> {
+                        Message in = exchange.getIn();
+                        in.setBody(new StringBuilder()
+                                .append("UPDATE expense SET amount = ")
+                                .append(in.getHeader("total"))
+                                .append(" WHERE sale_id = ")
+                                .append(in.getHeader("sale_id")).append(";"));
+                        in.setHeader("op", "u");
+                    })
+                    .log(LoggingLevel.DEBUG, "SQL statement: ${body}")
+                    .to("jdbc:cashback")
+                    .to("direct:kafka")
+                .end();
+
+        from("direct:kafka")
+                .routeId("direct2Kafka")
+                .process(exchange -> {
+                    Message in = exchange.getIn();
+                    Long saleId = in.getHeader("sale_id", Long.class);
+                    String op = in.getHeader("op", String.class);
+                    JsonObject json = new JsonObject().put("sale_id", saleId).put("op", op);
+                    in.setBody(json.toString());
+                    in.setHeader(KafkaConstants.KEY, saleId.toString());
+                    in.removeHeader("sale_id");
+                    in.removeHeader("op");
+                    in.removeHeader("total");
+                })
+                .to("kafka:{{kafka.expense_created.topic.name}}");
     }
 
     private String transformEpochToDateTime(long epoch) {
--- src/test/java/org/acme/retail/AggregatedSaleRouteTest.java
+++ src/test/java/org/acme/retail/AggregatedSaleRouteTest.java
@@ -1,9 +1,15 @@
 package org.acme.retail;
 
+import org.apache.camel.Exchange;
 import org.apache.camel.RoutesBuilder;
 import org.apache.camel.builder.AdviceWith;
+import org.apache.camel.component.kafka.KafkaConstants;
 import org.apache.camel.test.junit5.CamelTestSupport;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+import org.postgresql.util.PSQLException;
+import org.postgresql.util.PSQLState;
 
 public class AggregatedSaleRouteTest extends CamelTestSupport {
 
@@ -17,6 +23,9 @@ public class AggregatedSaleRouteTest extends CamelTestSupport {
             a.replaceFromWith("direct:start");
             a.weaveByToUri("jdbc:cashback").replace().to("mock:jdbc:cashback");
         });
+        AdviceWith.adviceWith(context, "direct2Kafka", a -> {
+            a.weaveByToUri("kafka:*").replace().to("mock:kafka:producer");
+        });
         context.start();
     }
 
@@ -26,9 +35,33 @@ public class AggregatedSaleRouteTest extends CamelTestSupport {
         adviceWithRoute();
 
         getMockEndpoint("mock:jdbc:cashback").expectedBodiesReceived("INSERT INTO expense (sale_id, customer_id, amount, date) VALUES (1000, 1001, 678.99, TIMESTAMP '2022-05-09 14:10:09');");
+        getMockEndpoint("mock:kafka:producer").expectedBodiesReceived("{\"sale_id\":1000,\"op\":\"c\"}");
         template.sendBody("direct:start", getAggregatedSaleEvent());
 
         assertMockEndpointsSatisfied();
+
+        Exchange ex = getMockEndpoint("mock:kafka:producer").assertExchangeReceived(0);
+        MatcherAssert.assertThat(ex.getIn().getHeaders().size(), Matchers.equalTo(1));
+        MatcherAssert.assertThat(ex.getIn().getHeader(KafkaConstants.KEY), Matchers.notNullValue());
+        MatcherAssert.assertThat(ex.getIn().getHeader(KafkaConstants.KEY).toString(), Matchers.equalTo("1000"));
+    }
+
+    @Test
+    public void testAggregatedSaleWithException() throws Exception {
+
+        adviceWithRoute();
+
+        getMockEndpoint("mock:jdbc:cashback").whenExchangeReceived(1, exchange -> {
+            throw new PSQLException("Error", PSQLState.UNKNOWN_STATE);
+        });
+
+        getMockEndpoint("mock:jdbc:cashback").expectedMessageCount(2);
+        getMockEndpoint("mock:kafka:producer").expectedBodiesReceived("{\"sale_id\":1000,\"op\":\"u\"}");
+        template.sendBody("direct:start", getAggregatedSaleEvent());
+
+        assertMockEndpointsSatisfied();
+        Exchange ex = getMockEndpoint("mock:jdbc:cashback").assertExchangeReceived(1);
+        MatcherAssert.assertThat(ex.getIn().getBody(String.class), Matchers.equalTo("UPDATE expense SET amount = 678.99 WHERE sale_id = 1000;"));
     }
 
     @Override
